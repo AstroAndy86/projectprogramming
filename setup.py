9200e772c08f5122f8a513d03a71b532cf19d4d5
Andrea Guzman Mesa <andru.0806@hotmail.com>
2017-01-20 11:03:23 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='astronomy',
+      version='1.0',
+      description='Weight and Age Converter',
+      author='Andrea Guzman Mesa',
+      author_email='andreauibk@gmail.com',
+      url='https://github.com/AstroAndy86/projectprogramming.git',
+      scripts=['astronomy']
+      
+     )
